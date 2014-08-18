873d43e978b1eee29a1ffdc34a0ec81b5d7dfa51
Ruchiranga <truchiranga@gmail.com>
2014-08-18 19:23:35 +0530

    Initial project work

---
+++ BBMS/src/bbmsv3/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bbmsv3;
+
+import de.javasoft.plaf.synthetica.SyntheticaBlackStarLookAndFeel;
+import gui.MedicalOfficer;
+import gui.Login;
+import gui.Nurse;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Anuradha
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
+            //UIManager.setLookAndFeel(new SyntheticaClassyLookAndFeel());
+            //MedicalOfficer bbms = new MedicalOfficer();
+            //bbms.setVisible(true);
+            Login bbms = new Login();
+            bbms.setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
